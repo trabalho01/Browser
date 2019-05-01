fc3a35604f79ec29fc9de68a60332e827dd1e30d
Daniel Ceriotti Trindade <danielceriottitrindade@gmail.com>
2019-05-01 18:47:50 -0300

    Alterações e inclusões realizadas na última aula conjuntamente com o professor(30/04)

---
+++ src/browser/Favoritos.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package browser;
+
+/**
+ *
+ * @author lucas
+ */
+public class Favoritos {
+    public void AddFavoritos (String endereco, int id_usr) {
+        //Select do banco de dados que mostre os favoritos do id.
+    }
+    
+    public void RemoveFavoritos(String endereco, int id_usr) {
+        //Update no BD
+    }
+}
